52399f4873546d203ce520f60f1517ed5fa7883a
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-06-11 18:31:30 +0530

    upto tasks.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
+import { useState } from 'react'
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 function App() {
+  const [tasks,setTasks] = useState(
+    [
+        {
+            id:1,
+            text:'tera mera sabka',
+            day:'Feb 5th at 2:30pm',
+            reminder:true,
+            
+        },
+        {
+            id:2,
+            text:'tera mera sabka',
+            day:'Feb 5th at 2:30pm',
+            reminder:false,
+            
+        },
+        {
+            id:3,
+            text:'tera mera sabka',
+            day:'Feb 5th at 2:30pm',
+            reminder:true,
+            
+        },
+        {
+            id:4,
+            text:'tera mera sabka',
+            day:'Feb 5th at 2:30pm',
+            reminder:false,
+            
+        }
+    ]
+);
   return (
     <div className='container'>
       <Header/>
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
